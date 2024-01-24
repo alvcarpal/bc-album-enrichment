b5b59f30739538b0a5dcf851e29bf6ad362d84e3
ACarmona <acarmona.contacto@gmail.com>
2024-01-24 10:28:59 +0100

    [BC-10] Fix LICENSE message

--- src/main/java/com/bc/album/infrastructure/db/springdata/mapper/PhotoDboMapper.java
+++ src/main/java/com/bc/album/infrastructure/db/springdata/mapper/PhotoDboMapper.java
@@ -3,17 +3,10 @@ package com.bc.album.infrastructure.db.springdata.mapper;
 import com.bc.album.domain.operational.Photo;
 import com.bc.album.infrastructure.db.springdata.model.PhotoDbo;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 /**
  * PhotoDboMapper interface. Mapstruct album mapper to domain.
  * In com.bc.album.infrastructure.db.springdata.mapper.
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so.
  *
  * @author Álvaro Carmona
  * @since 22/01/2024
